c71f5e8e5a7ca1d9957378306f3703c531bb587b
AndreaCapitelli <an.capitelli@studenti.unina.it>
2024-03-27 15:40:16 +0100

    il grande apporto di cap

--- Android/app/src/main/java/com/babbobastardo/ui/editGifters/GiftersActivity.java
+++ Android/app/src/main/java/com/babbobastardo/ui/editGifters/GiftersActivity.java
@@ -8,13 +8,25 @@ import com.babbobastardo.ui.ParentActivity;
 
 public class GiftersActivity extends ParentActivity {
     private ImageView snowFalling;
+    private ImageView doneBUtton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gifters_activity);
 
+
         snowFalling = findViewById(R.id.gifters_snowFalling);
+        doneButton =findViewById(gifters_doneButton);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
         startSnowFallingAnimation(snowFalling);
     }
 }
